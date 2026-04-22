99a42c10a392bbe77bf2983f0d7ebb68b41f9197
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-23 00:07:18 +0200

    Ristrukturim i kodit

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maison De Parfum</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/PerfumeStore_20/assets/css/style.css">
 </head>
 <body>
 
@@ -21,17 +21,16 @@
         <a href="/index.php">Home</a>
         <a href="/pages/products.php">Shop</a>
         <a href="/pages/about.php">About Us</a>
-        <a href="/pages/contact.php">Our Store</a>
 
         <div class="dropdown">
-            <span class="dropbtn">Account ▾</span>
+            <span class="dropbtn">Register ▾</span>
             <div class="dropdown-content">
                 <a href="/login.php">Log In</a>
                 <a href="#">Sign Up</a>
             </div>
         </div>
     </nav>
-
+ 
 </div>
 ```
 
