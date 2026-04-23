286aaf508669714f0caf03baeb7a74e6a1cea73f
ArijolaKrasniqi13 <arijola.krasniqi@student.uni-pr.edu>
2026-04-23 09:19:58 +0200

    vendosja e disa prdukteve

---
+++ includes/config.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../classes/Product.php");
+
+$products = [
+    new Product(1, "Dior Sauvage", 120, "men"),
+    new Product(2, "Bleu de Chanel", 140, "men"),
+    new Product(3, "Acqua di Gio", 110, "men"),
+    new Product(4, "Versace Eros", 100, "men"),
+    new Product(5, "Tom Ford Noir", 160, "men"),
+    
+
+    new Product(6, "Chanel No.5", 150, "women"),
+    new Product(7, "YSL Libre", 130, "women"),
+    new Product(8, "Gucci Bloom", 125, "women"),
+    new Product(9, "Dior J'adore", 145, "women"),
+    new Product(10, "Armani My Way", 135, "women"),
+
+    new Product(11, "CK One", 90, "unisex"),
+    new Product(12, "Maison Margiela Replica", 170, "unisex"),
+    new Product(13, "Le Labo Santal 33", 200, "unisex"),
+    new Product(14, "Byredo Gypsy Water", 180, "unisex"),
+    new Product(15, "Tom Ford Black Orchid", 155, "unisex")
+];
+
