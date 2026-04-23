4b312d331f9ed23e92e188692420f70a24acab47
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-23 20:12:48 +0200

    Rregullimi i linqeve

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,12 +6,12 @@
         <nav class="navigation">
             <a href="/PerfumeStore_20/index.php">Home</a>
             <a href="/PerfumeStore_20/pages/products.php">Shop</a>
-            <a href="#">About Us</a>
+            <a href="/PerfumeStore_20/pages/about.php">About Us</a>
 
             <div class="dropdown">
                 <span class="dropbtn">Register ▾</span>
                 <div class="dropdown-content">
-                    <a href="/PerfumeStore_20/pages/login.php">Log In</a>
+                    <a href="/PerfumeStore_20/login.php">Log In</a>
                     <a href="#">Sign Up</a>
                 </div>
             </div>
