e2dd00029d0070457204f1bdc10d4642d5704434
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-23 19:33:05 +0200

    Shtimi i footerit

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="container1">
+        <div class="row">
+
+            <!-- LOCATIONS -->
+            <div class="footer-col">
+                <h4>Locations</h4>
+                <ul>
+                    <li><a href="#">Tirane</a></li>
+                    <li><a href="#">Prishtine</a></li>
+                    <li><a href="#">Fushe Kosove</a></li>
+                    <li><a href="#">Prizren</a></li>
+                </ul>
+            </div>
+
+            <!-- CONTACT -->
+            <div class="footer-col">
+                <h4>Contact Us</h4>
+                <ul>
+                    <li><a href="#">Email: parfum@gmail.com</a></li>
+                    <li><a href="#">Phone: +355 69 253 6666</a></li>
+                    <li><a href="#">Fax: +355 69 253 6666</a></li>
+                </ul>
+            </div>
+
+            <!-- ABOUT -->
+            <div class="footer-col1">
+                <h4>Maison De Parfum</h4>
+                <ul>
+                    <li><a href="#">Your destination for luxury perfumes.</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <h3>© <?php echo date("Y"); ?>, Maison De Parfum | All rights reserved</h3>
+    </div>
+</footer>
+
+</body>
+</html>
