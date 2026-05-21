321373673548a89f440ee3938fcd373200ea110c
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-05-21 21:35:50 +0200

    Shtimi i validimit te formes

--- pages/edit-product.php
+++ pages/edit-product.php
@@ -46,6 +46,30 @@ $categories = mysqli_query(
      ORDER BY name ASC"
 );
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $name =
+        trim($_POST["name"] ?? "");
+
+    $price =
+        (float)($_POST["price"] ?? 0);
+
+    $categoryId =
+        (int)($_POST["category_id"] ?? 0);
+
+    if (
+        $name !== "" &&
+        $price >= 0 &&
+        $categoryId > 0
+    ) {
+
+    } else {
+
+        $message =
+            "Please fill all required fields.";
+    }
+}
+
 include("../includes/header.php");
 include("../includes/navbar.php");
 ?>
